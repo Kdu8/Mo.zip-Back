d6bdecb7f0c900036d2a4de058faf8094a5162c7
uuuuuuuk <s22034@gsm.hs.kr>
2022-12-19 09:43:37 +0900

    project start

---
+++ mozip/src/test/java/kdu8/mozip/MozipApplicationTests.java
@@ -0,0 +1,13 @@
+package kdu8.mozip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MozipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
