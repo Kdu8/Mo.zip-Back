12eba815964e4d8825246e1672cee9edf6350384
김원욱 <kimww0306@gmail.com>
2022-12-20 11:14:17 +0900

    Send Email Success

--- mozip/src/main/java/kdu8/mozip/presentation/dto/SendEmailRequest.java
+++ mozip/src/main/java/kdu8/mozip/presentation/dto/SendEmailRequest.java
@@ -4,10 +4,8 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import lombok.*;
 
 @Getter
-@JsonAutoDetect
 @AllArgsConstructor
+@NoArgsConstructor
 public class SendEmailRequest {
-
         private String email;
-
 }
--- mozip/src/main/java/kdu8/mozip/service/impl/EmailServiceImpl.java
+++ mozip/src/main/java/kdu8/mozip/service/impl/EmailServiceImpl.java
@@ -43,7 +43,7 @@ public class EmailServiceImpl implements EmailService {
         msgg+= ePw+"</strong><div><br/> ";
         msgg+= "</div>";
         message.setText(msgg, "utf-8", "html");//내용
-        message.setFrom(new InternetAddress("properties email쓰세용!","Babble"));//보내는 사람
+        message.setFrom(new InternetAddress("mozip12345@gmail.com","Mo.zip"));//보내는 사람
 
         return message;
     }
